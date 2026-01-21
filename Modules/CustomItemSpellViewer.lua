064d75d6318ec9d7ae7a3ff06323a1b05e45d754
Dale Hunt <dalehunt@gmail.com>
2026-01-21 10:50:11 +0000

    remove spell charges check for spells in item&spells bar, that's illegal

--- Modules/CustomItemSpellViewer.lua
+++ Modules/CustomItemSpellViewer.lua
@@ -189,12 +189,10 @@ local function CreateCustomSpellIcon(spellId)
             if spellCharges then
                 customIcon.Charges:SetText(tostring(spellCharges.currentCharges))
                 customIcon.Cooldown:SetCooldown(spellCharges.cooldownStartTime, spellCharges.cooldownDuration)
-                customIcon.Charges:SetAlphaFromBoolean(spellCharges.maxCharges > 1, 1, 0)
             else
                 local cooldownData = C_Spell.GetSpellCooldown(spellId)
                 customIcon.Cooldown:SetCooldown(cooldownData.startTime, cooldownData.duration)
                 customIcon.Charges:SetText("")
-                customIcon.Charges:SetAlpha(0)
             end
         end
     end)
